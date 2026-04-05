425bd684575d3a4e5050278b51917e597c0b115b
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2026-04-05 19:12:16 +0900

    1st commit

---
+++ src/test/java/test/study/leetcodeexplain/LeetcodeExplainApplicationTests.java
@@ -0,0 +1,13 @@
+package test.study.leetcodeexplain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeExplainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
